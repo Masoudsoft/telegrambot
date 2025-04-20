480919dfaa518b1605649c58c04d70ecb54ed714
Masoudsoft <soft.n1366@gmail.com>
2025-04-21 03:47:02 +0430

    اضافه کردن پاسخ به پیام‌های دریافتی

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 import telebot
 import os
 
-API_TOKEN = '8099196414:AAFUYCNnj9vq-h4MScsLPSuIcHNUzySWmQ0'  # 👈 
+API_TOKEN = '8099196414:AAFUYCNnj9vq-h4MScsLPSuIcHNUzySWmQ0' 
 
 bot = telebot.TeleBot(API_TOKEN)
 app = Flask(__name__)
@@ -24,3 +24,6 @@ def echo_all(message):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, "✅ پیام شما دریافت شد: " + message.text)
